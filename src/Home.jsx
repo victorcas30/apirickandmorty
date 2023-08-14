815af9ad06406e129673748e6e4b4eff511fb908
victorcas30 <vcastillo.mancia@gmail.com>
2023-08-14 11:11:41 -0600

    update home

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="row">
             <div className="col-md-4">
                 <div className="card project-card">
-                <img src="./src/assets/img/rick1.jpg" className="card-img-top" alt="Proyecto 2"/>
+                <img src="https://victorcas30.github.io/src/assets/img/rick1.jpg" className="card-img-top" alt="Proyecto 2"/>
                 <div className="card-body">
                     <h5 className="card-title">Proyecto 1</h5>
                     <p className="card-text">Tarjetas - Rick And Morty.</p>
