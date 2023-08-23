2e48b09d0663794c3544d38d4a4c3244acc0ad1f
victorcas30 <vcastillo.mancia@gmail.com>
2023-08-23 14:59:11 -0600

    restableciendo codigo

--- src/SubRutasRick.jsx
+++ src/SubRutasRick.jsx
@@ -1,79 +1,52 @@
-import { NavLink, Outlet, useMatch , useNavigate } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getAllPersonajesRickAndMorty } from "./leerApis";
-import "./style.css";
-import { useMediaQuery } from "react-responsive";
+import "./style.css"
 
 
 const SubRutasRick = () => {
-  const [personajes, setPersonajes] = useState([]);
-  const navigate = useNavigate(); // Hook para navegar
 
+    const [personajes,setPersonajes] = useState([]);
 
-  useEffect(() => {
-    getAllPersonajesRickAndMorty().then(getPersonajesRM => {
-      setPersonajes(getPersonajesRM);
-    });
-  }, []);
+    useEffect(() => {
+        getAllPersonajesRickAndMorty().then(getPersonajesRM => {
+            setPersonajes(getPersonajesRM);
+            console.table(personajes);
+        })
+    },[]);
 
-  const isMobile = useMediaQuery({ maxWidth: 767 }); // Detectar dispositivo móvil
-
-  const handleNameClick = (id) => {
-    // Navegar directamente a la tarjeta correspondiente
-    navigate(`/subrutasrick/cardricksubruta/${id}`);
-  };
-
-  return (
-    <>
-      <div className="container">
+    return(
+        <>
+        <div className="container">
         <h1 className="display-4">Sub Rutas Rick And Morty</h1>
-        <hr />
+        <hr/>
         <div className="container containerrutas">
-          <div className="columna1">
-            <div className="tableContainer">
-              <table className="table table-secondary">
-                <thead></thead>
-                <tbody>
-                  {personajes.map(personajesRick => {
-                    const { id, name } = personajesRick;
-                    return (
-                      <tr key={id}>
-                        <td>
-                          {isMobile ? (
-                            <button
-                              className="navLink"
-                              onClick={() => handleNameClick(id)}
-                            >
-                              {name}
-                            </button>
-                          ) : (
-                            <NavLink
-                              className={({ isActive }) =>
-                                isActive
-                                  ? "navLink activeNavLink"
-                                  : "navLink"
-                              }
-                              to={`cardricksubruta/${id}`}
-                            >
-                              {name}
-                            </NavLink>
-                          )}
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
+            <div className="columna1">
+                <div className="tableContainer">
+                <table className="table table-secondary">
+                    <thead></thead>
+                    <tbody>
+                    {
+                        personajes.map(personajesRick => {
+                            const {id,name} = personajesRick;
+                            return <tr key={id}>
+                                <td>
+                                    <NavLink className={({isActive}) => isActive ? "navLink activeNavLink" : "navLink"} to={`cardricksubruta/${id}`} >{name}</NavLink>
+                                </td>
+                            </tr>
+                        })
+                    }
+                    </tbody>
+                </table>
+                </div>
             </div>
-          </div>
-          <div className="columna2">
-          <Outlet />
-          </div>
+            <div className="columna2">
+                <Outlet/>
+            </div>
         </div>
-      </div>
-
-    </>
-  );
-};
+        </div>
+        </>
+    )
+}
 
 export default SubRutasRick;
